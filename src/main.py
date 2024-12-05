f78bc6ed72b66b0e7642d38d1bebd2cafa7b50a4
svndor-hub <sanzhar.d3@gmail.com>
2024-12-05 16:32:35 +0500

    add CORS middleware

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.randomuser.router import router as randomuser_router
 from src.auth.router import router as auth_router
@@ -8,3 +9,12 @@ app = FastAPI()
 app.include_router(randomuser_router, prefix="/randomuser")
 app.include_router(auth_router)
 app.include_router(users_router, prefix="/users")
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
